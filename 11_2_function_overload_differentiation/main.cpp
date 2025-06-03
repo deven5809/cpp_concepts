58aa3d6cc1966867d05fa427d0ae155292dfeca5
dan <deven5809@gmail.com>
2025-06-03 16:20:00 -0400

    Added an example for an ellipsis/variable argument list function

--- 11_2_function_overload_differentiation/main.cpp
+++ 11_2_function_overload_differentiation/main.cpp
@@ -1,6 +1,7 @@
 // This is a sample program intended to demonstrate the workings of function
 // overloading.
 #include <iostream>
+#include <cstdarg>
 #include "OverloadClass.hpp"
 
 // Uncomment the desired function to force the compiler to differentiate
@@ -8,7 +9,7 @@
 #define NUM_OF_PARAMS_EXAMPLE
 #define TYPE_OF_PARAMS_EXAMPLE
 // #define RETURN_TYPE_EXAMPLE
-#define CONST_EXAMPLE
+#define ELLIPSIS_EXAMPLE
 
 //==============================================================================
 // Function Declarations
@@ -40,6 +41,14 @@ float add(float num1, float num2);
  * on the return type. This is not allowed and will fail.
  */
 float add(int num1, int num2);
+#endif
+
+#ifdef ELLIPSIS_EXAMPLE
+/**
+ * @brief This demonstrates the compilers ability to differentiate ellipses from
+ * standard parameters
+ */
+int add(int num1, ...);
 #endif
 
 //==============================================================================
@@ -75,11 +84,40 @@ float add(int num1, int num2)
 }
 #endif
 
+#ifdef ELLIPSIS_EXAMPLE
+int add(int count, ...)
+{
+    std::cout << "Fifth add version called! (The variable argument list type)" << std::endl;
+    va_list list;
+    va_start(list, count);
+    int sum = 0;
+
+    for (int i = 0; i < count; ++i)
+    {
+        sum += static_cast<double>(va_arg(list, int));
+    }
+
+    va_end(list);
+
+    return sum;
+}
+#endif
+
 int main() {
     // Demonstrate the initial 3 examples
-    int sol = add(1, 2);
-    sol = add(1, 2, 3);
-    float float_solution = add(1.0f, 2.0f);
+    // This should call the first copy of the function.
+    int sol = add(1, 2); 
+
+    // This should call the second. Note that this was called with precedence 
+    // over the variable argument list version of the function! Very interesting.
+    sol = add(1, 2, 3); 
+
+    // And this should call the third.
+    float float_solution = add(1.0f, 2.0f); 
+
+    // This should call the variable argument list version
+    sol = add(5, 1, 2, 3, 4, 5);
+
 
     // Demonstrate how member functions of a class can be overloaded based on 
     // const or volatile qualifiers.
