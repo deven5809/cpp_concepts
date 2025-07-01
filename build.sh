7676ff54928b5217feaaad9284c3cce443d9ca94
dan <deven5809@gmail.com>
2025-07-01 18:11:40 -0500

    Added g++13 compiler for support for C++20

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@ if [ ! -d "out" ]; then
 fi
 cd out
 
-# Run CMake to generate a build system.
-cmake ../
+# Run CMake to generate a build system. We also want it to output a compile
+# commands json file for VS Code to parse.
+cmake ../ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 # Perform the build.
 cmake --build .
