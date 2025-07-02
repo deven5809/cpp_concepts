8a9d52ed4060c6b3afa94e631a771c1ffb134e08
dan <deven5809@gmail.com>
2025-07-01 19:21:34 -0500

    Completed section 11.9

--- 11_9_non_type_template_parameters/main.cpp
+++ 11_9_non_type_template_parameters/main.cpp
@@ -29,6 +29,10 @@
 // when using an improper function template argument in the example below.
 // #define DEMO_STATIC_ASSERT_ERROR
 
+// Uncomment this section to demonstrate how a constexpr int to constexpr double
+// conversion is not automatically allowed for a function template parameter.
+// #define DEMO_TYPE_CONVERSION_ERROR
+
 /**
  * @brief A trivial example of using an integer as a function template parameter. 
  */
@@ -54,14 +58,36 @@ double getSqrt()
     return 0.0;
 }
 
+/**
+ * @brief Since C++17, the auto keyword may be used in place of an actual type
+ * in order to indicate the the compiler should determine the type.
+ */
+template <auto N>
+void print_auto()
+{
+    std::cout << N << std::endl;
+}
+
 int main()
 {
     // The integer parameter is passed in to the function template section
     // instead of the function parameter section.
+    std::cout << "Printing the integer 5: ";
     print<5>();
+
+    // We can also see here that implicit casting can be performed on function
+    // template parameters in certain cases.
+    std::cout << "Printing the character 'a': ";
+    print<'a'>();
 
     // We do the same here for the getSqrt function.
     std::cout << "The square root of 5 is " << getSqrt<5.0>() << std::endl;
+
+#ifdef DEMO_TYPE_CONVERSION_ERROR
+    // Note here that if a constexpr int is passed where a constexpr double is
+    // expected, a promotion will not occur and an error will be generated.
+    std::cout << "The square root of 5 is " << getSqrt<5>() << std::endl;
+#endif // DEMO_TYPE_CONVERSION_ERROR
 
 #ifdef DEMO_STATIC_ASSERT_ERROR
     // This should fail the static assert in the getSqrt function which can be
@@ -69,4 +95,12 @@ int main()
     // constexpr.
     std::cout << "The square root of -5 is " << getSqrt<-5.0>() << std::endl;
 #endif // DEMO_STATIC_ASSERT_ERROR
+
+    // This section demonstrates the use of a function template with a non-type
+    // parameter specified with the type auto. In the example the type is 
+    // deduced by the compiler.
+    std::cout << "Printing the integer 5: ";
+    print_auto<5>();
+    std::cout << "Printing the character 'c': ";
+    print_auto<'c'>();
 }
